7137e72ef4986b31f4333fac07ff739228ba2787
Dinh Phong <dinhphong.developer@gmail.com>
2016-08-24 13:42:42 +0700

    fix bug

--- src/Rainy/Database/MySqlDatabase.php
+++ src/Rainy/Database/MySqlDatabase.php
@@ -6,11 +6,10 @@ use \Rainy\Database\Query\MySqlQuery as Query;
 
 class MySqlDatabase extends Database
 {
-<<<<<<< HEAD
     public function getQuery()
     {
         return new Query($this->pdo);
-=======
+
     public function table($table) {
         return new Query($this->pdo, $table, '');
     }
@@ -19,6 +18,6 @@ class MySqlDatabase extends Database
         $class = new Query($this->pdo, '', '');
 
         return $class->query($query);
->>>>>>> d216d895ce75b35839e3b938b97e2c1832f9804b
+
     }
 }
