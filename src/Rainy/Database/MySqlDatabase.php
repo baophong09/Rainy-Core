cd4933b9dfda24d5fef4f5d5a37eea3ab1df4a73
Dinh Phong <dinhphong.developer@gmail.com>
2016-08-24 13:44:31 +0700

    fix bug

--- src/Rainy/Database/MySqlDatabase.php
+++ src/Rainy/Database/MySqlDatabase.php
@@ -9,15 +9,5 @@ class MySqlDatabase extends Database
     public function getQuery()
     {
         return new Query($this->pdo);
-
-    public function table($table) {
-        return new Query($this->pdo, $table, '');
-    }
-
-    public function query($query) {
-        $class = new Query($this->pdo, '', '');
-
-        return $class->query($query);
-
     }
 }
