1791b9936453fbb7dc44c610e2b28fec37f4003d
Dinh Phong <dinhphong.developer@gmail.com>
2016-08-28 23:48:49 +0700

    Comment mysql database

--- src/Rainy/Database/MySqlDatabase.php
+++ src/Rainy/Database/MySqlDatabase.php
@@ -6,6 +6,14 @@ use \Rainy\Database\Query\MySqlQuery as Query;
 
 class MySqlDatabase extends Database
 {
+
+    /**
+     * get Query of this database
+     *
+     * @param null
+     *
+     * @return Object \Rainy\Database\Query
+     */
     public function getQuery()
     {
         return new Query($this->pdo);
